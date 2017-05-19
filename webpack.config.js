865465125d3dd71dfde77b85673d62fc69413270
akaidc2 <alex.kaidan@wendys.com>
2017-05-19 00:33:02 -0400

    720 minute update: updated to use webpack 2 in order to reduce load times. Build file size dropped from 6+ mb to under 1mb

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,199 @@
+const webpack = require('webpack');
 const path = require('path');
+
+const DashboardPlugin = require('webpack-dashboard/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const autoprefixer = require('autoprefixer');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const {getIfUtils, removeEmpty} = require('webpack-config-utils');
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-const {ifProduction, ifNotProduction} = getIfUtils(process.env.NODE_ENV)
+const nodeEnv = process.env.NODE_ENV || 'development';
+const isProduction = nodeEnv === 'prod';
+
+const jsSourcePath = path.join(__dirname, './app/js');
+const buildPath = path.join(__dirname, './build');
+const imgPath = path.join(__dirname, './app/static/images');
+const sourcePath = path.join(__dirname, './app');
+
+// Common plugins
+const plugins = [
+  new webpack.optimize.CommonsChunkPlugin({
+    name: 'vendor',
+    filename: 'vendor-[hash].js',
+    minChunks(module) {
+      const context = module.context;
+      return context && context.indexOf('node_modules') >= 0;
+    },
+  }),
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify(nodeEnv),
+    },
+  }),
+  new webpack.NamedModulesPlugin(),
+  new HtmlWebpackPlugin({
+    template: path.join(sourcePath, 'static/index.html'),
+    path: buildPath,
+    filename: 'index.html',
+  }),
+  new CleanWebpackPlugin(['build']),
+  new CopyWebpackPlugin([
+    {
+      context: path.resolve(__dirname, 'app/static'),
+      from: '**/*',
+      to: path.resolve(__dirname, 'build'),
+    },
+  ]),
+  new webpack.LoaderOptionsPlugin({
+    options: {
+      postcss: [
+        autoprefixer({
+          browsers: [
+            'last 3 version',
+            'ie >= 10',
+          ],
+        }),
+      ],
+      context: sourcePath,
+    },
+  }),
+];
+
+// Common rules
+const rules = [
+  {
+    test: /\.(js|jsx)$/,
+    exclude: /node_modules/,
+    use: [
+      'babel-loader',
+    ],
+  },
+  {
+    test: /\.(png|gif|jpg|svg)$/,
+    include: imgPath,
+    use: 'url-loader?limit=20480&name=assets/[name]-[hash].[ext]',
+  },
+  {
+  // Match woff2 in addition to patterns like .woff?v=1.1.1.
+  test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+  loader: 'url-loader',
+  options: {
+    // Limit at 50k. Above that it emits separate files
+    limit: 50000,
+
+    // url-loader sets mimetype if it's passed.
+    // Without this it derives it from the file extension
+    mimetype: 'application/font-woff',
+
+    // Output below fonts directory
+    name: './fonts/[name].[ext]',
+  }}
+];
+
+if (isProduction) {
+  // Production plugins
+  plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        screw_ie8: true,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true,
+      },
+      output: {
+        comments: false,
+      },
+    }),
+    new ExtractTextPlugin('style-[hash].css')
+  );
+
+  // Production rules
+  rules.push(
+    {
+      test: /\.scss$/,
+      loader: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: 'css-loader!sass-loader',
+      }),
+    }
+  );
+} else {
+  // Development plugins
+  plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+    new DashboardPlugin()
+  );
+
+  // Development rules
+  rules.push(
+    {
+      test: /\.scss$/,
+      exclude: /node_modules/,
+      use: [
+        'style-loader',
+        // Using source maps breaks urls in the CSS loader
+        // https://github.com/webpack/css-loader/issues/232
+        // This comment solves it, but breaks testing from a local network
+        // https://github.com/webpack/css-loader/issues/232#issuecomment-240449998
+        // 'css-loader?sourceMap',
+        'css-loader',
+        'sass-loader?sourceMap',
+      ],
+    }
+  );
+}
 
 module.exports = {
-  entry: path.resolve(__dirname, 'app/js/main'),
-  devServer: {
-    outputPath: path.join(__dirname, 'build'),
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx'],
+  devtool: isProduction ? false : 'source-map',
+  context: jsSourcePath,
+  entry: {
+    js: './main.jsx',
   },
   output: {
-    path: path.resolve(__dirname, 'build/js'),
-    publicPath: '/js/',
-    filename: 'bundle.js'
+    path: buildPath,
+    publicPath: '/',
+    filename: 'app-[hash].js',
   },
-  plugins: [
-    new CleanWebpackPlugin(['build']),
-    new CopyWebpackPlugin([
-      {
-        context: path.resolve(__dirname, 'app/static'),
-        from: '**/*',
-        to: path.resolve(__dirname, 'build'),
-      },
-    ]),
-  ],
-  devtool: '#eval-source-map',
   module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loaders: ['babel'],
-      },
-      {
-        test: /\.scss$/,
-        loaders: ['style', 'css', 'sass']
-      },
-      { test: /\.woff$/, loader: 'url?limit=65000&mimetype=application/font-woff&name=public/fonts/[name].[ext]' },
-      { test: /\.woff2$/, loader: 'url?limit=65000&mimetype=application/font-woff2&name=public/fonts/[name].[ext]' }
-    ]
-  }
-};
+    rules
 
-//rules: [{
-//  test: /\.scss$/,
-//  use: ifProduction(
-//    ExtractTextPlugin.extract({
-//      fallback: 'style-loader',
-//      use: ['css-loader', 'postcss-loader', 'sass-loader']
-//    }),
-//    ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
-//  )
-//}]
+  },
+  resolve: {
+    extensions: ['.webpack-loader.js', '.web-loader.js', '.loader.js', '.js', '.jsx'],
+    modules: [
+      path.resolve(__dirname, 'node_modules'),
+      jsSourcePath,
+    ],
+  },
+  plugins,
+  devServer: {
+    contentBase: isProduction ? buildPath : sourcePath,
+    historyApiFallback: true,
+    port: 3000,
+    compress: isProduction,
+    inline: !isProduction,
+    hot: !isProduction,
+    host: '0.0.0.0',
+    stats: {
+      assets: true,
+      children: false,
+      chunks: false,
+      hash: false,
+      modules: false,
+      publicPath: false,
+      timings: true,
+      version: false,
+      warnings: true,
+      colors: {
+        green: '\u001b[32m',
+      },
+    },
+  },
+};
